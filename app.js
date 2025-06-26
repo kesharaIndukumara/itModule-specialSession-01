fa430c8ebe75c1714b7d0cba1dbd1c5faf8ace6a
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-06-26 09:49:11 +0530

    js-function-types

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 console.log("Hello, console!!");
-
+// step-01
 // function getSum(x,y){
 //     return x+y;
 // }
-
 // console.log(getSum(10,20));
 
-let getSum = function(x,y){
-    return x+y;
-}
-console.log(getSum(10,20));
+// step-02
+// let getSum = function(x,y){
+//     return x+y;
+// }
+// console.log(getSum(10,20));
+
+// step-03
+// let getSum = (x,y) =>{
+//     return x+y;
+// }
+// console.log(getSum(10,20));
+
+// step-04
+let getSum = (x+y) => x+y;
