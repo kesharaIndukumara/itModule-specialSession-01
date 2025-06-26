51feefd863cfbb2484143f543384f343d34dcd1c
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-06-26 09:38:17 +0530

    special-session-basics of js oparators

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hello, console!!");
+
+// function getSum(x,y){
+//     return x+y;
+// }
+
+// console.log(getSum(10,20));
+
+let getSum = function(x,y){
+    return x+y;
+}
+console.log(getSum(10,20));
